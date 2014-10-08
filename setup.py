c9634877d63c93251a850ec946653133b51e3da1
dan <dan.bate@gmail.com>
2014-10-08 16:48:01 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='django-model-monitor',
+    version='0.0.1',
+    packages=['modelmonitor'],
+    package_dir={'': 'src'},
+    url='https://github.com/OmegaDroid/django-model-monitor',
+    license='MIT',
+    author='Daniel Bate',
+    author_email='',
+    description='App to monitor changes in models'
+)
